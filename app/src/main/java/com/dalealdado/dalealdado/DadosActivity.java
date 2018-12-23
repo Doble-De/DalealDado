9a2d66ec5186a6fd0dc1d3998edc9407444c952c
Doble-De <ddelgados96@elpuig.xeill.net>
2018-12-23 18:15:13 +0100

    Dados a punto de acabar

--- app/src/main/java/com/dalealdado/dalealdado/DadosActivity.java
+++ app/src/main/java/com/dalealdado/dalealdado/DadosActivity.java
@@ -2,6 +2,11 @@ package com.dalealdado.dalealdado;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -10,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.InterruptedException;
 
@@ -17,7 +23,10 @@ import java.util.Random;
 
 public class DadosActivity extends AppCompatActivity {
     Random r = new Random();
-
+    MediaPlayer mp;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     String[] cien = {"00", "10", "20", "30", "40", "50", "60", "70", "80", "90"};
     TextView tirar6;
@@ -43,6 +52,7 @@ public class DadosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados);
+        mp = MediaPlayer.create(this, R.raw.tirar);
 
         tirar6 = findViewById(R.id.dado6tv);
         tirar4 = findViewById(R.id.dado4tv);
@@ -52,12 +62,47 @@ public class DadosActivity extends AppCompatActivity {
         tirar20 = findViewById(R.id.dado20tv);
         tirar100 = findViewById(R.id.dado100tv);
         tirar100u = findViewById(R.id.dadosptv);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+//                tvShake.setText("Shake Action is just detected!!");
+                Toast.makeText(DadosActivity.this, "Tirando dados", Toast.LENGTH_SHORT).show();
+
+                mp.start();
+                tirarDado6(tirar6);
+                tirarDado4(tirar4);
+                tirarDado8(tirar8);
+                tirarDado10(tirar10);
+                tirarDado12(tirar12);
+                tirarDado20(tirar20);
+                tirarDado100(tirar100);
+                tirarDado100u(tirar100u);
+               // valor6.postValue(r.nextInt(6) + 1);
+//
+//            serviceActivity.tirar6.setText(String.valueOf(valor6));
+
+               // int color = Color.argb(255, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+               // tirar6.setText(String.valueOf(valor6));
+            }
+        });
+
+
     }
 
     public void tirarDado6(View view) {
+        mp.start();
         final TextView tirar6 = findViewById(R.id.dado6tv);
-
-
 
         final Handler handler = new android.os.Handler();
         final Runnable runnable = new Runnable() {
@@ -83,6 +128,7 @@ public class DadosActivity extends AppCompatActivity {
 
 
     public void tirarDado4(View view) {
+        mp.start();
         final TextView tirar4 = findViewById(R.id.dado4tv);
 
         final Handler handler = new android.os.Handler();
@@ -108,6 +154,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado8(View view) {
+        mp.start();
         final TextView tirar8 = findViewById(R.id.dado8tv);
 
         final Handler handler = new android.os.Handler();
@@ -133,6 +180,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado10(View view) {
+        mp.start();
         final TextView tirar10 = findViewById(R.id.dado10tv);
 
         final Handler handler = new android.os.Handler();
@@ -158,6 +206,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado12(View view) {
+        mp.start();
         final TextView tirar12 = findViewById(R.id.dado12tv);
 
         final Handler handler = new android.os.Handler();
@@ -183,6 +232,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado20(View view) {
+        mp.start();
         final TextView tirar20 = findViewById(R.id.dado20tv);
 
         final Handler handler = new android.os.Handler();
@@ -208,6 +258,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado100(View view) {
+        mp.start();
         final TextView tirar100 = findViewById(R.id.dado100tv);
 
         final Handler handler = new android.os.Handler();
@@ -233,6 +284,7 @@ public class DadosActivity extends AppCompatActivity {
     }
 
     public void tirarDado100u(View view) {
+        mp.start();
         final TextView tirar100u = findViewById(R.id.dadosptv);
 
         final Handler handler = new android.os.Handler();
@@ -255,6 +307,20 @@ public class DadosActivity extends AppCompatActivity {
                 tirar100u.setText(String.valueOf(integer));
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
 
