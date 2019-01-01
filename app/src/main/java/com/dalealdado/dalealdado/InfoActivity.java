25f9f7668fc02498b081694775ae91a1dd169a9d
Doble-De <ddelgados96@elpuig.xeill.net>
2019-01-01 22:41:01 +0100

    Retocando...

--- app/src/main/java/com/dalealdado/dalealdado/InfoActivity.java
+++ app/src/main/java/com/dalealdado/dalealdado/InfoActivity.java
@@ -2,6 +2,9 @@ package com.dalealdado.dalealdado;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        Glide.with(this).load(R.drawable.lobi).into((ImageView)findViewById(R.id.imagenlobi));
+        Glide.with(this).load(R.drawable.lobi).into((ImageView)findViewById(R.id.imagendados));
     }
+
 }
--- app/src/main/java/com/dalealdado/dalealdado/MainActivity.java
+++ app/src/main/java/com/dalealdado/dalealdado/MainActivity.java
@@ -1,27 +1,32 @@
 package com.dalealdado.dalealdado;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mp = MediaPlayer.create(this, R.raw.minicio);
+        mp.start();
         findViewById(R.id.bdados).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent (MainActivity.this, DadosActivity.class) );
+                mp.stop();
             }
         });
         findViewById(R.id.info).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent (MainActivity.this, InfoActivity.class) );
+                mp.stop();
             }
         });
 
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, PersonajesActivity.class));
+                mp.stop();
             }
         });
     }
